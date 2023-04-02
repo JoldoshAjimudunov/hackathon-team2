6a572d5e32cb81ff4745a07ac311d241876c0594
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-04-02 09:58:14 +0600

    deploy4

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import AuthContextProvider from "./components/contexts/AuthContextProvider";
 import ChatContextProvider from "./components/contexts/ChatContextProvider";
 import ProfileContextProvider from "./components/contexts/ProfileContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthContextProvider>
       <ChatContextProvider>
         <ProfileContextProvider>
@@ -17,5 +17,5 @@ root.render(
         </ProfileContextProvider>
       </ChatContextProvider>
     </AuthContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
